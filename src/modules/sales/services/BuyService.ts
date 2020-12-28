d96e05c2af94d70ad625eb97666944a694d1dcc1
recabp <renatocabp@outlook.com>
2020-12-28 13:15:52 -0300

    Notifications

--- src/modules/sales/services/BuyService.ts
+++ src/modules/sales/services/BuyService.ts
@@ -21,7 +21,6 @@ interface IRequest {
   client_id: string;
   type: 'client' | 'provider';
 
-
 }
 
 
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -11,12 +11,14 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 import IUserTokensReposytory from '@modules/users/repositories/IUserTokensRepository';
 
-
 import StockRepository from '@modules/sales/infra/typeorm/repositories/StockRepository';
 import IStockRepository from '@modules/sales/repositories/IStockRepository';
 
 import PurchaseRepository from '@modules/sales/infra/typeorm/repositories/PurchaseRepository';
 import IPurchaseRepository from '@modules/sales/repositories/IPurchaseRepository';
+
+import InotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import NotificationsReposytory from '@modules/notifications/infra/typeorm/repositories/NotificationsRepository';
 
 
 
@@ -36,5 +38,8 @@ container.registerSingleton<IStockRepository>(
 
 container.registerSingleton<IPurchaseRepository>(
   'PurchaseRepository', PurchaseRepository);
+
+container.registerSingleton<InotificationsRepository>(
+  'NotificationsRepository', NotificationsReposytory);
 
 
